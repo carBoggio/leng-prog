1fb43efbc014c0b23844562a5a890e2c7c150a8f
Carla Boggio <carlaboggio123@gmail.com>
2024-08-05 17:01:51 -0300

    make 2 tps

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,11 @@
 module CollatzConjecture (collatz) where
 
+collatzFun :: Integer -> Integer -> Maybe Integer
+collatzFun n iteration
+  | (n <= 0) = Nothing
+  | (n == 1) = Just iteration
+  | ((n `mod` 2) == 1) =  collatzFun (n * 3 + 1) (iteration + 1)
+  | otherwise = collatzFun (n `div` 2) (iteration + 1)
+
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n =  collatzFun n 0
--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -5,27 +5,35 @@ type Fraction = (Int, Int)
 -- Implement the `add` Function
 
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
-
+add (num1, denom1) (num2, denom2)
+  | denom1 == denom2 = simpFunc (num2+num1, denom1)
+  | otherwise = simpFunc (num1 * denom2 + num2 * denom1 , denom1 * denom2)
 -- Implement the `sub` Function
 
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub (num1, denom1) (num2, denom2)
+  | denom1 == denom2 = simpFunc (num2-num1, denom1)
+  | otherwise = simpFunc (num1 * denom2 - num2 * denom1 , denom1 * denom2)
+
 
 -- Implement the `mul` Function
 
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul (num1, denom1) (num2, denom2) = simpFunc (num1 * num2, denom1 * denom2)
 
 -- Implement the `divide` Function
 
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide (num1, denom1) (num2, denom2) = simpFunc (mul (num1, denom1) (denom2, num2))
 
 -- Implement the `hcf` Function
 
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
+hcf a b
+  | (b == 0) = a
+  | otherwise = hcf b (a `mod` b)
 
+simpFunc:: Fraction -> Fraction
+simpFunc (n1, n2) = (n1  `div` (hcf n1 n2), n2  `div` (hcf n1 n2))
 
-    
+  
