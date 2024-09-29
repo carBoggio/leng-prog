d7aa3a66ac87b4351a8db3b7759ce27b36476e33
Carla Boggio <carlaboggio123@gmail.com>
2024-09-29 19:19:00 -0300

    finish the tree

--- haskell-exercises/trie/src/Trie.hs
+++ haskell-exercises/trie/src/Trie.hs
@@ -1,20 +1,48 @@
 module Trie  (Trie(..), left, right, find, decode, toList) where
-
+import Control.Exception (Exception)
 import Bit
-  
-data Trie a = Complete -- Add the Type definition deriving (Eq, Show)
-            
+import Control.Exception (throw)
+data TrieExceptionLeft = LeftOfException String deriving (Show)
+instance Exception TrieExceptionLeft
+
+
+data Trie a = Leaf a -- El leaf es un constructor del Trie a
+              | Trie a :-: Trie a deriving (Eq, Show) -- El :-: es un constructor del Trie a
+
+
 left::Trie a -> Trie a
-left = error "Define it"
+left (Leaf _) = throw $ LeftOfException "Left of: Leaf"
+left (x :-: _) = x
 
 right::Trie a -> Trie a
-left = error "Define it"
+right (Leaf _) = throw $ LeftOfException "Right of: Leaf"
+right (_ :-: x) = x
   
 find::Bits -> Trie a -> a
-find = error "Define it"
+find [] (Leaf x) = x
+find [] (_ :-: _) = throw $ LeftOfException "Find: Empty bits"
+find (x: sx) trie
+  | x == F = find sx (left trie)
+  | otherwise = find sx (right trie)
+
 
 decode::Bits -> Trie Char -> String
-decode = error "Define it"
+decode x trie = decodeSubFunc x "" trie trie
+
+
+decodeSubFunc::Bits -> String -> Trie Char -> Trie Char-> String
+decodeSubFunc [] str (Leaf a) _ = str ++ [a]
+decodeSubFunc x string (Leaf a) completeTrie = decodeSubFunc x (string ++ [a]) completeTrie completeTrie
+decodeSubFunc (x: sx) string (l :-: r) completeTrie
+  |(x == F) = decodeSubFunc sx string l completeTrie
+  | otherwise = decodeSubFunc sx string r completeTrie
+
 
 toList::Trie a -> [(a, Bits)]
-toList = error "Define it"
+toList trie = preOrder trie [] []
+
+
+
+preOrder:: Trie a -> Bits -> [(a, Bits)] -> [(a, Bits)]
+preOrder (Leaf x) bits list = list ++ [(x, bits)]
+preOrder (l :-: r) bits list = preOrder l (bits ++ [F]) list ++ preOrder r (bits ++ [T] ) list
